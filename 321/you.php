c8a616f6f05079490a82a137523d63bba7f8bd04
Administrator <123>
2019-01-10 10:24:14 +0800

    123

---
+++ 321/you.php
@@ -0,0 +1,8 @@
+<?php
+header("Access-Control-Allow-Origin : http://10.30.29.77:63341");
+header("Access-Control-Allow-Credentials : true");
+header('Access-Control-Allow-Headers : Content-Type');
+echo '{"userName":"123"}';
+////$data=array("userName"=>"456664646");
+//echo json_encode($data);
+?>

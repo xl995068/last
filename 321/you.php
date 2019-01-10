aaaa10173c7ebbfd9ae84100f020f80d89928627
Administrator <123>
2019-01-10 10:33:01 +0800

    123

--- 321/you.php
+++ 321/you.php
@@ -2,7 +2,23 @@
 header("Access-Control-Allow-Origin : http://10.30.29.77:63341");
 header("Access-Control-Allow-Credentials : true");
 header('Access-Control-Allow-Headers : Content-Type');
-echo '{"userName":"123"}';
+$str=file_get_contents('php://input');
+var_dump($str);
+var_dump(json_decode($str));
+
+$username=json_decode($str).$userName;
+$password=json_decode($str).$password;
+
+if($username=='123' && $password=='123'){
+    $data=[
+        'message'=>'success'
+    ];
+}else{
+    $data=[
+        'message'=>'error'
+    ];
+}
+echo json_decode($data)
 ////$data=array("userName"=>"456664646");
 //echo json_encode($data);
 ?>
